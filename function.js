04760af978fec4cb5d9f550b594a3e354a17503d
z5590213 <z5590213@vx23.orchestra.cse.unsw.EDU.AU>
2025-02-11 14:53:14 +1100

    Added files to structure the program out

---
+++ function.js
@@ -0,0 +1,2 @@
+// This file will contain all the functions that will check the strength of the password
+import { commonPasswords } from './commpass.js';
---
+++ main.js
@@ -0,0 +1,2 @@
+// Main program logic, which calls the functions and prints the report
+import { commonPasswords } from './commpass.js';
